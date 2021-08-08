267ef35d84a31d90c134652b15f0eda04d039f98
nyakamwe <nyakamweaimable@gmail.com>
2021-08-08 22:41:05 +0200

    and contact logic

---
+++ Pages/Movies/Contact.cshtml.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using CineMania.Data;
+using CineMania.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CineMania.Pages.Movies
+{
+    public class ContactModel : PageModel
+    {
+        #region context config
+        private readonly CinemaniaContext _context;
+        public ContactModel(CinemaniaContext context)
+        {
+            _context = context; 
+        }
+
+        #endregion
+        
+
+        #region saving
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        [BindProperty] 
+        public Message Message { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+        
+            _context.Messages.Add(Message);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index" );
+            
+        }
+
+        #endregion
+        
+    }
+}
